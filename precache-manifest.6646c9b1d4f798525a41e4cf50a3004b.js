0351e4f4d8db55c33d693ac7a697ec0008e7122a
Vincent M <vincent.monard@free.fr>
2018-10-18 14:21:20 +0200

    Updates

---
+++ precache-manifest.6646c9b1d4f798525a41e4cf50a3004b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "bad52d09861a08baa1b4",
+    "url": "/lucy/static/js/6.bad52d09.chunk.js"
+  },
+  {
+    "revision": "24471a605c50c7a573a5",
+    "url": "/lucy/static/js/0.24471a60.chunk.js"
+  },
+  {
+    "revision": "595270b35ec65d26e7fc",
+    "url": "/lucy/static/js/main.595270b3.chunk.js"
+  },
+  {
+    "revision": "6775dd636594610b254e",
+    "url": "/lucy/static/js/2.6775dd63.chunk.js"
+  },
+  {
+    "revision": "6989437eb70d107f9bc6",
+    "url": "/lucy/static/js/3.6989437e.chunk.js"
+  },
+  {
+    "revision": "8a738438b08493189d7b",
+    "url": "/lucy/static/js/4.8a738438.chunk.js"
+  },
+  {
+    "revision": "daf9b4bf1478ed1a5f1a",
+    "url": "/lucy/static/js/5.daf9b4bf.chunk.js"
+  },
+  {
+    "revision": "bc86174da7bce634b0f0c2fb907d65b4",
+    "url": "/lucy/static/media/LeClubPetanque.bc86174d.enc"
+  },
+  {
+    "revision": "960d668d48b3bdff0aed",
+    "url": "/lucy/static/js/runtime~main.960d668d.js"
+  },
+  {
+    "revision": "4cc4ec4f344b5af180d9836d879fd551",
+    "url": "/lucy/static/media/logo.4cc4ec4f.png"
+  },
+  {
+    "revision": "57310a2564192ade839364887c56ad0a",
+    "url": "/lucy/static/media/background.57310a25.png"
+  },
+  {
+    "revision": "bc4ef9150680ceddd34c8c998b29c57e",
+    "url": "/lucy/static/media/21.bc4ef915.enc"
+  },
+  {
+    "revision": "0ba7b6196e5f79bd771fbfcc283f3d26",
+    "url": "/lucy/static/media/Lagoon.0ba7b619.enc"
+  },
+  {
+    "revision": "595270b35ec65d26e7fc",
+    "url": "/lucy/static/css/main.43c91612.chunk.css"
+  },
+  {
+    "revision": "4f5ef0a55bec3c7b4ea0503ae22884f1",
+    "url": "/lucy/index.html"
+  }
+];
